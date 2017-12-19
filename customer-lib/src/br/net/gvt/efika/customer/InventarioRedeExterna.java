4325a8630d1938f65564b30f607438170f1ece7e
Henrique Merlin <henmerlin@gmail.com>
2017-12-19 18:13:12 -0200

    add Inventario Rede externa attrs

--- customer-lib/src/br/net/gvt/efika/customer/InventarioRedeExterna.java
+++ customer-lib/src/br/net/gvt/efika/customer/InventarioRedeExterna.java
@@ -10,6 +10,9 @@ public class InventarioRedeExterna {
     private OrigemRede origem;
     private OrigemPlanta planta;
 
+    private String splitter1n, splitter2n, caboAlim;
+    private String fibra1n, fibra2n;
+
     public InventarioRedeExterna() {
     }
 
@@ -35,6 +38,46 @@ public class InventarioRedeExterna {
 
     public void setPlanta(OrigemPlanta planta) {
         this.planta = planta;
+    }
+
+    public String getSplitter1n() {
+        return splitter1n;
+    }
+
+    public void setSplitter1n(String splitter1n) {
+        this.splitter1n = splitter1n;
+    }
+
+    public String getSplitter2n() {
+        return splitter2n;
+    }
+
+    public void setSplitter2n(String splitter2n) {
+        this.splitter2n = splitter2n;
+    }
+
+    public String getCaboAlim() {
+        return caboAlim;
+    }
+
+    public void setCaboAlim(String caboAlim) {
+        this.caboAlim = caboAlim;
+    }
+
+    public String getFibra1n() {
+        return fibra1n;
+    }
+
+    public void setFibra1n(String fibra1n) {
+        this.fibra1n = fibra1n;
+    }
+
+    public String getFibra2n() {
+        return fibra2n;
+    }
+
+    public void setFibra2n(String fibra2n) {
+        this.fibra2n = fibra2n;
     }
 
 }
