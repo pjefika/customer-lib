7e362fbd015a7703f525967bd8c9620401da11ef
Henrique Merlin <henmerlin@gmail.com>
2018-03-15 16:47:30 -0300

    fix

--- customer-lib/src/main/java/br/net/gvt/efika/customer/model/enums/SystemEnum.java
+++ customer-lib/src/main/java/br/net/gvt/efika/customer/model/enums/SystemEnum.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package br.net.gvt.efika.customerAPI.model.enums;
+package br.net.gvt.efika.customer.model.enums;
 
 import com.fasterxml.jackson.annotation.JsonValue;
 
